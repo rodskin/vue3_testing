8a09689053352c2d735d7d56abbb0b8e042b0429
fgilles <fgilles@corp.free.fr>
2021-05-14 14:57:47 +0200

    test dynamic pages

--- src/views/page.vue
+++ src/views/page.vue
@@ -2,15 +2,22 @@
     <div class="page">
         <h1>test page</h1>
         
-        <component :is="pageLoaded"></component>
+        <component :is="pageLoaded"/>
     </div>
 </template>
 
 <script>
+    import test from '@/pages/start'
+    
     export default {
         name: 'page',
         components: {
-            pageLoaded: () => import('@/pages/start.vue')
+            test
+        },
+        data() {
+            return {
+                pageLoaded: "test"
+            }
         }
     }
 </script>
